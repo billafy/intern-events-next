c780046b6ca06d925cb767f2d454a9fbe13092ab
unknown <ytarkar138@gmail.com>
2021-09-01 14:35:26 +0530

    test-config-jest

---
+++ __tests__/Home.test.js
@@ -0,0 +1,11 @@
+import {render, screen} from '@testing-library/react'
+
+import Home from '../pages/index'
+
+describe('Home tests', () => {
+	it('should render a heading with text `Home`', () => {
+		render(<Home/>)
+		const heading = screen.getByRole('heading', {name: /home/i})
+		expect(heading).toBeInTheDocument()
+	})
+})
