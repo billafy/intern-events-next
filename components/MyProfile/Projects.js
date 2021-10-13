839c1ba84cf2a3ec5c86b19b4e8da02c54e6daa6
Yash Tarkar <ytarkar138@gmail.com>
2021-10-13 14:06:14 +0530

    'myprofile-delete-button'

--- components/MyProfile/Projects.js
+++ components/MyProfile/Projects.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import _ from "../../styles/MyProfile.module.scss";
 import Popup from "../Popup";
 import { MdAdd } from "react-icons/md";
 import { reqPut } from "../../utils/customRequests";
 import urls from "../../utils/urls";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Projects = ({}) => {
 	const dispatch = useDispatch();
@@ -20,19 +21,27 @@ const Projects = ({}) => {
 	const [seeMore, setSeeMore] = useState(false);
 	const limit = 3;
 
+	const updateAccountByProjects = (newProjects) => {
+		return {
+			...account,
+			details: { ...account.details, projects: newProjects },
+		};
+	};
+
 	const addProject = async (event) => {
 		event.preventDefault();
-		console.log(account);
 		const data = await reqPut(urls.updateAccount + account._id, {
-			account: {
-				...account,
-				details: {
-					...account.details,
-					projects: [...account.details.projects, project],
-				},
-			},
+			account: updateAccountByProjects([
+				...account.details.projects,
+				project,
+			]),
 		});
 		if (data.success) {
+			setProject({
+				title: "",
+				link: "",
+				description: "",
+			});
 			setShowProjects(false);
 			dispatch({
 				type: "UPDATE_ACCOUNT",
@@ -41,17 +50,35 @@ const Projects = ({}) => {
 		}
 	};
 
+	const deleteProject = async (_id) => {
+		const newProjects = account.details.projects.filter(
+			(proj) => proj._id !== _id
+		);
+		const data = await reqPut(urls.updateAccount + account._id, {
+			account: updateAccountByProjects(newProjects),
+		});
+		if (data.success)
+			dispatch({
+				type: "UPDATE_ACCOUNT",
+				payload: { account: data.body.account },
+			});
+	};
+
 	return (
 		<div className={_.projects}>
 			<h3>Projects</h3>
 			<div>
 				<ul>
 					{account.details.projects.map((proj, index) => {
-						if(!seeMore && index >= limit) 
-							return <></>
+						if (!seeMore && index >= limit) return <Fragment key={proj._id}></Fragment>;
 						return (
-							<li key={index}>
-								<h4>{proj.title}</h4>
+							<li key={proj._id}>
+								<h4>
+									{proj.title}{" "}
+									<AiOutlineClose
+										onClick={() => deleteProject(proj._id)}
+									/>
+								</h4>
 								<p>{proj.description}</p>
 								<a href={proj.link} target="_blank">
 									View Project
@@ -60,11 +87,14 @@ const Projects = ({}) => {
 						);
 					})}
 				</ul>
-				{account.details.projects.length >= limit && 
-					<p onClick={() => setSeeMore(!seeMore)} className={_.seeMore}>
-						{seeMore ? 'See Less' : 'See More'}
+				{account.details.projects.length > limit && (
+					<p
+						onClick={() => setSeeMore(!seeMore)}
+						className={_.seeMore}
+					>
+						{seeMore ? "See Less" : "See More"}
 					</p>
-				}
+				)}
 			</div>
 			<Popup
 				button={<MdAdd />}
--- components/MyProfile/Skills.js
+++ components/MyProfile/Skills.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import _ from "../../styles/MyProfile.module.scss";
 import Popup from "../Popup";
 import { MdAdd } from "react-icons/md";
 import { reqPut } from "../../utils/customRequests";
-import urls from '../../utils/urls';
+import urls from "../../utils/urls";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Skills = () => {
 	const {
@@ -16,18 +17,23 @@ const Skills = () => {
 	const [seeMore, setSeeMore] = useState(false);
 	const limit = 5;
 
+	const updateAccountBySkills = (newSkills) => {
+		return {
+			...account,
+			details: { ...account.details, skills: newSkills },
+		};
+	};
+
 	const addSkill = async (event) => {
 		event.preventDefault();
 		const data = await reqPut(urls.updateAccount + account._id, {
-			account: {
-				...account,
-				details: {
-					...account.details,
-					skills: [...account.details.skills, skill],
-				},
-			},
+			account: updateAccountBySkills([
+				...account.details.skills,
+				{ title: skill },
+			]),
 		});
 		if (data.success) {
+			setSkill("");
 			setShowSkills(false);
 			dispatch({
 				type: "UPDATE_ACCOUNT",
@@ -36,22 +42,42 @@ const Skills = () => {
 		}
 	};
 
+	const deleteSkill = async (_id) => {
+		const newSkills = account.details.skills.filter(
+			(skl) => skl._id !== _id
+		);
+		const data = await reqPut(urls.updateAccount + account._id, {
+			account: updateAccountBySkills(newSkills),
+		});
+		if (data.success)
+			dispatch({
+				type: "UPDATE_ACCOUNT",
+				payload: { account: data.body.account },
+			});
+	};
+
 	return (
 		<div className={_.skills}>
 			<h3>Skills</h3>
 			<div>
 				<ul>
 					{account.details.skills.map((skl, index) => {
-						if(!seeMore && index >= limit) 
-							return <></>
-						return <li key={index}>{skl}</li>
+						if (!seeMore && index >= limit) return <Fragment key={skl._id}></Fragment>;
+						return (
+							<li key={skl._id}>
+								<h4>{skl.title} <AiOutlineClose onClick={() => deleteSkill(skl._id)}/></h4>
+							</li>
+						);
 					})}
 				</ul>
-				{account.details.skills.length >= limit && 
-					<p onClick={() => setSeeMore(!seeMore)} className={_.seeMore}>
-						{seeMore ? 'See Less' : 'See More'}
+				{account.details.skills.length > limit && (
+					<p
+						onClick={() => setSeeMore(!seeMore)}
+						className={_.seeMore}
+					>
+						{seeMore ? "See Less" : "See More"}
 					</p>
-				}
+				)}
 			</div>
 			<Popup
 				button={<MdAdd />}
